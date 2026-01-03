958f803bc62c12085ad1b8fdafacbb6b6e14d673
Safwan <solaimansafwan@gmail.com>
2026-01-03 22:17:08 +0600

    Finished 1.4 by adding inputs instead of automated movement

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
-
+    public float speed = 10.0f;
+    public float turnSpeed = 50.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -15,10 +17,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
         // Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        
-
+        // Turn the vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
